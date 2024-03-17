1d495008b9f7f0d730b24a06023629ed4a166db3
Patris08 <144215350+Patris08@users.noreply.github.com>
2024-03-17 20:38:52 +0100

    Update settings.py

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
  #   os.path.join(BASE_DIR, 'static'),
 #]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
